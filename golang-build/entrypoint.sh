f55decd599d925d0ac29df5a698675a407d19a36
Eric Florenzano <floguy@gmail.com>
2019-09-09 19:00:36 -0700

    Add support for ARM

--- golang-build/entrypoint.sh
+++ golang-build/entrypoint.sh
@@ -15,7 +15,7 @@ fi
 root_path="/go/src/github.com/$GITHUB_REPOSITORY"
 release_path="$GITHUB_WORKSPACE/.release"
 repo_name="$(echo $GITHUB_REPOSITORY | cut -d '/' -f2)"
-targets=${@-"darwin/amd64 darwin/386 linux/amd64 linux/386 windows/amd64 windows/386"}
+targets=${@-"darwin/amd64 darwin/386 linux/amd64 linux/386 linux/arm/5 linux/arm/6 linux/arm/7 windows/amd64 windows/386"}
 
 echo "----> Setting up Go repository"
 mkdir -p $release_path
@@ -26,10 +26,11 @@ cd $root_path
 for target in $targets; do
   os="$(echo $target | cut -d '/' -f1)"
   arch="$(echo $target | cut -d '/' -f2)"
-  output="${release_path}/${repo_name}_${os}_${arch}"
+  arm="$(echo $target | cut -d '/' -f3)"
+  output="${release_path}/${repo_name}_${os}_${arch}${arm}"
 
   echo "----> Building project for: $target"
-  GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -o $output
+  GOOS=$os GOARCH=$arch GOARM=$arm CGO_ENABLED=0 go build -o $output
   zip -j $output.zip $output > /dev/null
 done
 
